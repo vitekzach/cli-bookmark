c27829abe9c457f2f59386327ce1b8d5ad8fee94
vitekzach <vitekzach@users.noreply.github.com>
2024-01-07 15:17:59 +0100

    improve config error handling, add panics

--- src/config.go
+++ src/config.go
@@ -16,43 +16,66 @@ type configValues struct {
 }
 
 type Config interface {
-	readconfigvalues() configValues
-	updateconfigvalues(configValues)
+	saveconfigvalues()
 }
 
-func (c *configValues) readconfigvalues() {
+func readconfigvalues() configValues {
+	// TODO what if config doesn't exist yet?
 	Logger.Debug("Loading config values.")
+
+	// create default config
+	var conf configValues
+	conf.Version = "0.0.1"
+
+	if _, err := os.Stat(configFilePath); os.IsNotExist(err) {
+		Logger.Info("Config file does not exist, saving a default one.")
+		conf.saveconfigvalues()
+		return conf
+		// TODO save config
+	}
+	// TODO make a config backup first
+
+	Logger.Debug("Config file exists, loading it")
 
 	configJson, err := os.ReadFile(configFilePath)
 
 	if err != nil {
 		Logger.Error("Config couldn't be read from a file", "error", err)
-		// TODO error here
+		panic(fmt.Sprintf("Your config file located in %v cannot be read, fix or remove it.", configFilePath))
 	}
 
-	err = json.Unmarshal(configJson, &c)
+	err = json.Unmarshal(configJson, &conf)
 	if err != nil {
 		Logger.Error("Config couldn't unmarshaled from JSON", "error", err)
-		// TODO error here
+		panic(fmt.Sprintf("Your config file located in %v cannot be parsed as JSON, fix or remove it.", configFilePath))
 	}
 
 	Logger.Debug("Config loaded")
+
+	return conf
 }
 
-func (c configValues) updateconfigvalues(conf configValues) {
+func (c *configValues) saveconfigvalues() {
 	Logger.Debug("Saving config values.")
-	configJson, err := json.Marshal(conf)
+	configJson, err := json.Marshal(c)
 
 	if err != nil {
 		Logger.Error("Config couldn't be converted to json", "error", err)
-		// TODO error here
+		fmt.Printf("WARNING: Error while saving config file (cannot marshal JSON), your settings will not be persisted.")
+		// TODO make it red?
+		//TODO reference log
+		//TODO reference github
 	}
 
 	err = os.WriteFile(configFilePath, configJson, 0666)
 
 	if err != nil {
 		Logger.Error(fmt.Sprintf("Config couldn't be saved to config path %v", configFilePath), "error", err)
+		fmt.Printf("WARNING: Cannot save config file, your settings will not be persisted.")
 		// TODO error here
+		// TODO make it red
+		//TODO reference log
+		//TODO reference github
 	}
 
 	Logger.Debug("Config saved")
@@ -104,9 +127,11 @@ func GetConfig() {
 		Logger.Info("Config folder created")
 	}
 
-	config := configValues{}
-	config.readconfigvalues()
-	config.updateconfigvalues(config)
+	config := readconfigvalues()
+
+	// config.updateconfigvalues(config)
 
 	Logger.Info(fmt.Sprintf("Loaded config for app version %v", config.Version))
+	Logger.Info(fmt.Sprintf("Config app version %v", config.Version))
+	config.saveconfigvalues()
 }
